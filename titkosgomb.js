d7d624a2248165df7b8adbd90612184fb444e714
Roncz Olivér <90604012+Oliverpartequattro@users.noreply.github.com>
2023-04-19 12:50:59 +0200

    mindennek vége működik a gomb Kiss Gábor #5 #17

---
+++ titkosgomb.js
@@ -0,0 +1,29 @@
+const checkInput = (e) => {
+    if (e.target.value == "Kiss Gábor") {
+        e.target.classList.remove('error')
+        e.target.classList.add('success')
+        submitBtn.removeAttribute('disabled')
+        inputDisplay.textContent = args.success.value
+    } else {
+        e.target.classList.add('error')
+        e.target.classList.remove('success')
+        submitBtn.setAttribute('disabled', 'true')
+        inputDisplay.textContent = args.error.value
+    }
+}
+
+const args = {
+    success: {
+        value: ""
+    },
+    error: {
+        value: ""
+    }
+}
+
+const fname = document.getElementById('fname')
+const submitBtn = document.getElementById('submit')
+const inputDisplay = document.getElementById('input-display')
+inputDisplay.textContent = args.error.value
+submitBtn.disabled = 'true'
+fname.addEventListener('input', checkInput)
