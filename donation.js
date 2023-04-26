27e86af3e02d848327038dc16ae8d4f0299b43b0
Fazekas Botond 9.C <90604573+fazekasbotond3@users.noreply.github.com>
2023-04-26 12:12:58 +0200

    #12 #14 #8

---
+++ donation.js
@@ -0,0 +1,5 @@
+function MyFunction() {
+
+    HTMLButtonElement.onclick().alert("Hálásan köszönjük támogatását!\nMinden beérkező fityinget a kutatásunk fejlesztésére fogunk felhasználni(VAGY NEM).")
+
+}
