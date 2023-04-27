b0e231b0e6f860fbc97cd67fe6adf64c0bdc8139
Fazekas Botond 9.C <90604573+fazekasbotond3@users.noreply.github.com>
2023-04-27 09:39:52 +0200

    #8 javascript

--- donation.js
+++ donation.js
@@ -1,5 +1,10 @@
-function MyFunction() {
-
-    HTMLButtonElement.onclick().alert("Hálásan köszönjük támogatását!\nMinden beérkező fityinget a kutatásunk fejlesztésére fogunk felhasználni(VAGY NEM).")
-
+function myFunction() {
+    let text;
+    let person = prompt("Kérem írjon egy rövid véleményt kutatásunkról!", "SZKUBIDUBIDOOOO");
+    if (person == null || person == "") {
+        text = "Nem írtál semmit.\nSZÉGYELLD MAGAD!!!";
+    } else {
+        text = "Hello " + person + "! How are you today?";
+    }
+    document.getElementById("demo").innerHTML = text;
 }
